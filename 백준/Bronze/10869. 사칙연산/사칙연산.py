da579c9b0251fe73f910cb6fcffd96cc76388f0e
song ju hee <80883709+jhsong76@users.noreply.github.com>
2022-06-15 23:30:51 +0900

    [Bronze V] Title: 사칙연산, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,6 +1,8 @@
 a, b = input().split()
-print(int(a) + int(b))
-print(int(a) - int(b))
-print(int(a) * int(b))
-print(int(a) // int(b))
-print(int(a) % int(b))
+
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(a)//int(b))
+print(int(a)%int(b))
+
